73ddaa5b1c4bbad5e71ef2286ca74b2cf716492b
rachmanj <rachmanj@gmail.com>
2026-02-17 22:38:57 +0800

    Fix InputNumber parser type

--- components/savings/savings-accounts-list.tsx
+++ components/savings/savings-accounts-list.tsx
@@ -377,7 +377,7 @@ export function SavingsAccountsList() {
                   min={1}
                   placeholder="0"
                   formatter={(v) => `${v}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-                  parser={(v) => parseInt((v ?? "").replace(/\D/g, ""), 10) || 0}
+                  parser={(v: string | undefined) => parseInt((v ?? "").replace(/\D/g, ""), 10) || 0}
                 />
               </Form.Item>
               <Form.Item name="reference_number" label="No. Referensi">
